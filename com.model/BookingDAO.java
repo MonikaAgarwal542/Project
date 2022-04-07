07aebfa908f1d88381714ffdbc78e9b639f5606e
MonikaAgarwal542 <84187029+MonikaAgarwal542@users.noreply.github.com>
2022-04-07 21:23:35 +0530

    Delete com.model directory

--- com.model/BookingDAO.java
+++
@@ -1,9 +0,0 @@
-package com.model;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public interface BookingDAO {
-	public Payment makePayment(int id);
-	public Booking cancelBooking(int id);
-}
--- com.model/FlightDAO.java
+++
@@ -1,8 +0,0 @@
-package com.model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface FlightDAO  extends CrudRepository<Flight,Integer> {
-
-}
