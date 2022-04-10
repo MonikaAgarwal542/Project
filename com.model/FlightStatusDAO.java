7f76f8e3a5935f659d7bcd38070f670ad0d9a373
MonikaAgarwal542 <84187029+MonikaAgarwal542@users.noreply.github.com>
2022-04-10 19:08:14 +0530

    Delete com.model directory

--- com.model/FlightStatusDAO.java
+++
@@ -1,9 +0,0 @@
-package com.model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FlightStatusDAO  extends CrudRepository<FlightStatus,Integer>{
-
-}
